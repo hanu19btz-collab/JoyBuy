9e969016a6e6459b3b5b8968fc1ec5af1c545e0f
Claudiu <hanu19btz@gmail.com>
2026-05-20 22:30:27 +0100

    Connected frontend to live backend

--- frontend/app.js
+++ frontend/app.js
@@ -428,15 +428,14 @@ uploadBtn.addEventListener(
 
         try {
 
-            const response =
-                await fetch(
-                    `http://127.0.0.1:8000/upload?depot=${currentDepot.id}`,
-                    {
-                        method: 'POST',
-                        body: formData
-                    }
-                );
-
+           const response =
+    await fetch(
+        `https://joybuy-backend.onrender.com/upload?depot=${currentDepot.id}`,
+        {
+            method: 'POST',
+            body: formData
+        }
+    );
             stopsData =
                 await response.json();
 
